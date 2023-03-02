af276345922d8efb7dfa5fff7bd1b9830bb675e2
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    config: Conditionally enable dev keys signing

--- config/common.mk
+++ config/common.mk
@@ -278,6 +278,20 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     ImmersiveNavigationOverlay
     
+# Signing
+ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard .android-certs/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard .android-certs/verity.pk8))
+PRODUCT_VERITY_SIGNING_KEY := .android-certs/verity
+endif
+ifneq (,$(wildcard .android-certs/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+endif
+endif
+
 # pixelstar prebuilts
 $(call inherit-product, vendor/pixelstar-prebuilts/config.mk)
 
