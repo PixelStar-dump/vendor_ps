82c085bfe1a60fc7cd7850da19df3e0405f32400
John Galt <johngaltfirstrun@gmail.com>
2023-11-14 12:34:06 -0500

    signing: support inline signing for U [2/2]
    For those who have used .android-certs previously, cp .android-certs/* vendor/everest/signing/keys/

--- config/common.mk
+++ config/common.mk
@@ -280,12 +280,12 @@ PRODUCT_PACKAGES += \
     
 # Signing
 ifeq (user,$(TARGET_BUILD_VARIANT))
-ifneq (,$(wildcard .android-certs/releasekey.pk8))
-PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+ifneq (,$(wildcard vendor/pixelstar/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/pixelstar/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
-ifneq (,$(wildcard .android-certs/otakey.x509.pem))
-PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+ifneq (,$(wildcard vendor/pixelstar/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/pixelstar/signing/keys/otakey.x509.pem
 endif
 endif
 
