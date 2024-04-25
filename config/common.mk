1b6179394ef0e192f4cd25c890159decacde9276
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-04-26 00:09:33 +0530

    [SQUASH]:rro_overlays: Update for qpr2 - Add Pixel overlays from henteios - Switch certification repo - Do not build build_manifest - Build TouchGestures - Update configs from pixys - Add  pixelstar-prebuilts
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Co-authored-by: Mudit200408 <muditpurohit2004/pm@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -85,9 +85,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
-# Gapps
-$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
-
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
 
@@ -139,9 +136,9 @@ TARGET_SCREEN_HEIGHT ?= 1920
 PRODUCT_PACKAGES += \
     bootanimation.zip
 
-# Build Manifest
+# TouchGestures
 PRODUCT_PACKAGES += \
-    build-manifest
+    TouchGestures
 
 # BtHelper
 PRODUCT_PACKAGES += \
@@ -295,8 +292,17 @@ $(call inherit-product, vendor/pixelstar/config/bootanimation.mk)
 
 include vendor/pixelstar/config/branding.mk
 
-#Certification
-$(call inherit-product-if-exists, vendor/certification/config.mk)
+# Certification
+include vendor/certification/BoardConfig.mk
+
+# rro_overlays
+$(call inherit-product, vendor/pixelstar/config/rro_overlays.mk)
+
+$(call inherit-product, vendor/gms/products/gms.mk)
+$(call inherit-product, vendor/pixelstar/config/pixel-rro_overlays.mk)
+
+# pixelstar prebuilts
+$(call inherit-product, vendor/pixelstar-prebuilts/config.mk)
 
 # Enable ThinLTO Source wide Conditionally.
 ifeq ($(TARGET_BUILD_WITH_LTO),true)
