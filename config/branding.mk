a0cbc4043ff758a2fcb3e42c008f58d950b4c5e9
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-05-02 12:54:23 +0530

    bump to 2.0

--- config/branding.mk
+++ config/branding.mk
@@ -15,8 +15,8 @@
 # limitations under the License.
 
 # Versioning System
-PIXELSTAR_CODENAME := uday
-PIXELSTAR_NUM_VER := 1.3
+PIXELSTAR_CODENAME := unnati
+PIXELSTAR_NUM_VER := 2.0
 
 TARGET_PRODUCT_SHORT := $(subst pixelstar_,,$(PIXELSTAR_BUILD_TYPE))
 
